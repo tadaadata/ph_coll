86340f3bbfd286154bbebdc84a00af07e400a70b
jemus42 <jemus42@quantenbrot.de>
2018-05-07 15:40:05 +0200

    Trimming w04 to realism

--- woche04/woche04.R
+++ woche04/woche04.R
@@ -1,8 +1,12 @@
 # Der Game of Thrones (Books only) Datensatz
 # https://data.tadaa-data.de/gotdeaths_books.rds
-library(readr)
+library("readr")
 
+# von lokal
 gotbooks <- read_rds("daten/gotdeaths_books.rds")
+
+# von internet
+gotbooks <- read_rds(url("https://data.tadaa-data.de/gotdeaths_books.rds"))
 
 # Erste paar Zeilen angucken
 head(gotbooks)
@@ -12,7 +16,7 @@ unique(gotbooks$Allegiances)
 
 # Wir stellen fest: Duplikate ("House Stark" + "Stark") :(
 
-library(dplyr)
+library("dplyr")
 
 gotbooks %>%
   group_by(Allegiances) %>%
@@ -20,7 +24,7 @@ gotbooks %>%
 
 
 # Saubermachen: Allegiances ----
-library(stringr)
+library("stringr")
 
 # Ohne speichern, nur gucken (Zuweisung in gotbooks)
 gotbooks %>%
@@ -28,12 +32,28 @@ gotbooks %>%
 
 # Mit speichern
 gotbooks <- gotbooks %>%
-  mutate(Allegiances = str_remove(Allegiances, "House\\ "))
+  mutate(Allegiances = str_remove(Allegiances, pattern = "House\\ ")) # regex
 
 # Nochmal angucken
 gotbooks %>%
   group_by(Allegiances) %>%
-  tally()
+  tally() %>%
+  arrange(desc(n)) # arrange DESCending (absteigend sortieren)
+
+# identisch aber kürzer
+gotbooks %>%
+  count(Allegiances) %>%
+  arrange(desc(n))
+
+
+library("ggplot2")
+
+gotbooks %>%
+  count(Allegiances) %>%
+  ggplot(aes(x = reorder(Allegiances, n), y = n)) +
+  geom_col() +
+  coord_flip()
+
 
 # Rekodieren ----
 unique(gotbooks$Gender) # 0 = Weiblich; 1 = Männlich
@@ -42,6 +62,9 @@ unique(gotbooks$Nobility)
 
 gotbooks <- gotbooks %>%
   mutate(Gender = recode_factor(Gender, `0` = "Female", `1` = "Male"))
+
+# dasselbe wie:
+gotbooks$Gender <- recode_factor(gotbooks$Gender, `0` = "Female", `1` = "Male")
 
 # Gender ist jetzt ein factor
 gotbooks$Gender
@@ -66,6 +89,8 @@ sjt.xtab(gotbooks$Nobility, gotbooks$Gender)
 # Spaltennamen ----
 names(gotbooks)
 
+library("stringr")
+
 # Zum ausprobieren
 names(gotbooks) %>%
   str_replace_all(pattern = " ", replacement = "_") %>%
@@ -77,50 +102,3 @@ names(gotbooks) <- names(gotbooks) %>%
   str_to_lower()
 
 names(gotbooks)
-
-# Wide und Long format
-library(tidyr)
-
-head(gotbooks)
-
-# Wir haben 5 Spalten, je eine pro Buch, die anzeigen ob eine Person im Buch vorkommt
-# Ziel: Zwei Spalten. Eine "Buchtitel", eine "Person taucht auf ja/nein"
-
-gotbooks %>%
-  gather(key = "book", value = "appearance", got, cok, sos, ffc, dwd)
-
-# gather = "fasse zusammen"
-# Variable "book" (key) entählt die alten Variablennamen (got, cok, sos, ffc, dwd)
-# Variable appearance (value) enthält die alten Werte (0 oder 1)
-# Der resultierence Datenstaz ist deutlich länger! 5 Zeilen pro Person, eine Zeile pro Buch
-
-# Speichern
-gotbooks_long <- gotbooks %>%
-  gather(key = "book", value = "appearance", got, cok, sos, ffc, dwd)
-
-# Jetzt sind wir flexibler
-gotbooks_long %>%
-  group_by(book) %>%
-  summarize(appearances_sum = sum(appearance))
-
-# Plots? ----
-library(ggplot2)
-
-ggplot(data = gotbooks_long, aes(x = book, y = appearance)) +
-  geom_col()
-
-# Sortiert nach Häufigkeit
-# Gruppieren nach "book", "appearance" zählen
-gotbooks_long %>%
-  group_by(book) %>%
-  tally(appearance)
-
-# Piping von dplyr direkt in ggplot stuff
-gotbooks_long %>%
-  group_by(book) %>%
-  tally(appearance) %>%
-  ggplot(aes(x = reorder(book, n), y = n)) +
-  geom_col() +
-  labs(title = "A Song of Ice and Fire",
-       subtitle = "Characters per Book",
-       x = "Book", y = "Character Appearances")
